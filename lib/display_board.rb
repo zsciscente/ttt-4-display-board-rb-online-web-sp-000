6bd6b11890a8a2afb547e3beea0e3d375b4cba7a
Zachary Sciscente <zsciscente@gmail.com>
2020-07-16 15:42:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,7 +11,7 @@ def display_board(board)
   puts "-----------"
   puts "  #{board[3]} | #{board[4]}  | #{board[5]}  "
   puts "-----------"
-  puts "  #{board[6]} | #{board[7]}  |  #{board[8]} "
+  puts " #{board[6]} | #{board[7]}  |  #{board[8]} "
   
 end
 
